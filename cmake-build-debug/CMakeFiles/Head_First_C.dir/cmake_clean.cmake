994ce07e511dfa434a5078c2a0dbab8cd87b5c1a
Shuowei <googleearth@berkeley.edu>
2019-05-21 16:41:40 -0700

    hello

---
+++ cmake-build-debug/CMakeFiles/Head_First_C.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Head_First_C.dir/main.c.o"
+  "Head_First_C.pdb"
+  "Head_First_C"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Head_First_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
